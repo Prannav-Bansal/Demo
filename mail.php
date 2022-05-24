a8a61530c3c4c919c18e2abaacd8b8212b817211
Prannav-Bansal <105575423+Prannav-Bansal@users.noreply.github.com>
2022-05-24 15:46:52 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['emailaddress'];
+$message= $_POST['message'];
+$number= $_POST['mobile'];
+$to = "pranavbansal0310@gmail.com";
+$subject = "Mail From Brix Website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message."\r\n
+Mobile number =". $number;
+
+$headers = "From: noreply@pranavb.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
